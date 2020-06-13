4efceac15daf28a71a20efb356619838277d5f57
Mark Johnson <markthomasjohnson@gmail.com>
2020-06-13 18:56:46 -0400

    Updates

--- content.js
+++ content.js
@@ -5,3 +5,10 @@ chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message, sender, sendResponse) {
     console.log(message.txt);
 }
+
+
+// function siteCheck(site,action) {
+//     if (site == bad) {
+//         console.log("Negative Site Detective")
+//     }
+// }
